e02c9b741492a95bdd36cc4a7f43d5a1b771863c
matao <muxter.matao@gmail.com>
2017-03-24 15:25:16 +0800

    [REV] Modularization

--- module2/src/main/java/com/muxter/module2/Module2Activity.java
+++ module2/src/main/java/com/muxter/module2/Module2Activity.java
@@ -1,13 +1,13 @@
-package com.muxter.module1;
+package com.muxter.module2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Module2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_module2);
     }
 }
